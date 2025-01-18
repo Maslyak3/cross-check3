c525b500b591366588852ace975fa537e68092e6
lia5 <liaforweb@gmail.com>
2025-01-18 20:32:33 +0200

    add: task 1 read file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+// Import required modules
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath, 'utf8');
+
+readStream.on('data', data => {
+  process.stdout.write(data);
+});
